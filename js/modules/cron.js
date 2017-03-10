a021b8c3da7c2cc50c415b8a98d8320f9b6052b3
own3mall <own3mall@gmail.com>
2017-03-09 23:16:38 -0700

    JS Fix

--- js/modules/cron.js
+++ js/modules/cron.js
@@ -9,8 +9,11 @@ $(document).ready(function(){
 function checkSteamSupportAutoUpdate(elem){
 	var curOpt = $("option:selected", $(elem));
 	if(curOpt.attr('steam')){
-		$("option[value='steam_auto_update']", $("select[name='action']")).removeAttr('disabled');
+		$("option[value='steam_auto_update']", $("select[name='action']", $(elem).parent().prev())).removeAttr('disabled');
 	}else{
-		$("option[value='steam_auto_update']", $("select[name='action']")).attr('disabled','disabled');
+		$("option[value='steam_auto_update']", $("select[name='action']", $(elem).parent().prev())).attr('disabled','disabled');
+		if($("option[value='steam_auto_update']", $("select[name='action']", $(elem).parent().prev())).is(':selected')){
+			$("select[name='action'] option:enabled:first", $(elem).parent().prev()).prop('selected', 'selected').change();
+		}
 	}
 }
