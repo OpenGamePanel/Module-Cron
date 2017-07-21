022e971ba460ccc4850e494325ec08de6440c45c
own3mall <own3mall@gmail.com>
2017-07-21 06:57:47 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Persian/modules/cron.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('minute', "Minute");
+define('hour', "Hour");
+define('day', "Day");
+define('month', "Month");
+define('day_of_the_week', "D/Week");
+define('action', "Action");
+define('steam_auto_update', "Steam Auto Update");
+define('stop', "Stop");
+define('start', "Start");
+define('restart', "Restart");
+define('command', "Command");
+define('add', "Add");
+define('remove', "Remove");
+define('now', "Now");
+define('schedule_new_job', "Schedule a new job");
+define('scheduled_jobs', "Scheduled jobs");
+define('there_are_no_scheduled_jobs', "There are no scheduled jobs");
+define('cron_events', "Cron Events");
+define('refresh_interval', "Refresh Interval");
+define('cron_no_servers_tied_to_account', "There are no game servers directly assigned to your account.");
+define('cron_admin_link_display_text', "Cron Administration (All Servers)");
+define('cron_admin_no_ogp_servers_to_display', "There aren't any game servers currently configured in OGP.");
+?>
