9c0ec59a958e55ee71605a9f653be8378bcc6841
own3mall <own3mall@gmail.com>
2017-09-14 00:49:53 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Portuguese(Brazil)/modules/cron.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('minute', "Minuto");
+define('hour', "Hora");
+define('day', "Dia");
+define('month', "Mês");
+define('day_of_the_week', "D/Semana");
+define('action', "Ação");
+define('steam_auto_update', "Atualização automática do Steam");
+define('stop', "Pare");
+define('start', "Iniciar");
+define('restart', "Reiniciar");
+define('command', "Comando");
+define('add', "Adicionar");
+define('remove', "Remover");
+define('now', "Agora");
+define('schedule_new_job', "Agendar um novo trabalho");
+define('scheduled_jobs', "Trabalhos agendados");
+define('there_are_no_scheduled_jobs', "Não existem trabalhos agendados");
+define('cron_events', "Tarefas Cron");
+define('refresh_interval', "Intervalo de atualização");
+define('cron_no_servers_tied_to_account', "Não há servidores de jogos diretamente atribuídos à sua conta.");
+define('cron_admin_link_display_text', "Administração de Cron (todos os servers)");
+define('cron_admin_no_ogp_servers_to_display', "Não há nenhum servidor de jogos atualmente configurado no painel.");
+?>
