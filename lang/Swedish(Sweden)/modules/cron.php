5ecd8eba08e27fa91e178a1ea1084aef01f7fa17
own3mall <own3mall@gmail.com>
2017-12-07 17:08:42 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Swedish(Sweden)/modules/cron.php
+++ lang/Swedish(Sweden)/modules/cron.php
@@ -22,26 +22,26 @@
  *
  */
 
-define('minute', "Minute");
-define('hour', "Hour");
-define('day', "Day");
-define('month', "Month");
-define('day_of_the_week', "D/Week");
-define('action', "Action");
-define('steam_auto_update', "Steam Auto Update");
-define('stop', "Stop");
-define('start', "Start");
-define('restart', "Restart");
-define('command', "Command");
-define('add', "Add");
-define('remove', "Remove");
-define('now', "Now");
-define('schedule_new_job', "Schedule a new job");
-define('scheduled_jobs', "Scheduled jobs");
-define('there_are_no_scheduled_jobs', "There are no scheduled jobs");
-define('cron_events', "Cron Events");
-define('refresh_interval', "Refresh Interval");
-define('cron_no_servers_tied_to_account', "There are no game servers directly assigned to your account.");
-define('cron_admin_link_display_text', "Cron Administration (All Servers)");
-define('cron_admin_no_ogp_servers_to_display', "There aren't any game servers currently configured in OGP.");
+define('OGP_LANG_minute', "Minut");
+define('OGP_LANG_hour', "Timma");
+define('OGP_LANG_day', "Dag");
+define('OGP_LANG_month', "Månad");
+define('OGP_LANG_day_of_the_week', "Dag i Veckan");
+define('OGP_LANG_action', "Handling");
+define('OGP_LANG_steam_auto_update', "Steam autouppdatera");
+define('OGP_LANG_stop', "Stop");
+define('OGP_LANG_start', "Start");
+define('OGP_LANG_restart', "Omstart");
+define('OGP_LANG_command', "Kommando");
+define('OGP_LANG_add', "Lägg till");
+define('OGP_LANG_remove', "Ta bort");
+define('OGP_LANG_now', "Nu");
+define('OGP_LANG_schedule_new_job', "Schemalägg ett nytt jobb");
+define('OGP_LANG_scheduled_jobs', "Schemalagda jobb");
+define('OGP_LANG_there_are_no_scheduled_jobs', "Det finns inga schemalagda jobb");
+define('OGP_LANG_cron_events', "Cron händelser");
+define('OGP_LANG_refresh_interval', "Uppdaterinsintervall");
+define('OGP_LANG_cron_no_servers_tied_to_account', "Det finns inga spelservrar direkt tilldelade till ditt konto.");
+define('OGP_LANG_cron_admin_link_display_text', "Cron administration (Alla Servrar)");
+define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "Det finns just nu inga spelservrar konfigurerade i OGP.");
 ?>
