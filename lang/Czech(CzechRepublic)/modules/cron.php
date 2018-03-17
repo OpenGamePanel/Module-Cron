ad3270ae538c2dbf80b90e914d54fb77d3c44a53
own3mall <own3mall@gmail.com>
2018-03-17 15:28:52 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Czech(CzechRepublic)/modules/cron.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_minute', "Minuta");
+define('OGP_LANG_hour', "Hodina");
+define('OGP_LANG_day', "Den");
+define('OGP_LANG_month', "Měsíc");
+define('OGP_LANG_day_of_the_week', "Den / Týden");
+define('OGP_LANG_action', "Akce");
+define('OGP_LANG_steam_auto_update', "Steam Auto Update");
+define('OGP_LANG_stop', "Vypnout");
+define('OGP_LANG_start', "Zapnout");
+define('OGP_LANG_restart', "Restartovat");
+define('OGP_LANG_command', "Příkaz");
+define('OGP_LANG_add', "Přidat");
+define('OGP_LANG_remove', "Odstranit");
+define('OGP_LANG_now', "Nyní");
+define('OGP_LANG_schedule_new_job', "Naplánujte novou úlohu");
+define('OGP_LANG_scheduled_jobs', "plánované úlohy");
+define('OGP_LANG_there_are_no_scheduled_jobs', "Neexistují žádné naplánované úlohy");
+define('OGP_LANG_cron_events', "cron Ulohy");
+define('OGP_LANG_refresh_interval', "Interval obnovení");
+define('OGP_LANG_cron_no_servers_tied_to_account', "Na váš účet nejsou přímo přiřazeny žádné herní servery.");
+define('OGP_LANG_cron_admin_link_display_text', "Cron Administrace (všechny servery)");
+define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "V OGP nejsou aktuálně nakonfigurovány žádné herní servery.");
+define('OGP_LANG_bad_inputs', "Plánovaný čas obsahuje neplatné znaky.");
+?>
