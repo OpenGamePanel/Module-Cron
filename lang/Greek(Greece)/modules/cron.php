ab39cd5fe3ca53d72d8eec592841a41f20e61fc5
own3mall <own3mall@gmail.com>
2018-05-27 09:31:54 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Greek(Greece)/modules/cron.php
+++ lang/Greek(Greece)/modules/cron.php
@@ -22,27 +22,27 @@
  *
  */
 
-define('OGP_LANG_minute', "Minute");
-define('OGP_LANG_hour', "Hour");
-define('OGP_LANG_day', "Day");
-define('OGP_LANG_month', "Month");
-define('OGP_LANG_day_of_the_week', "D/Week");
-define('OGP_LANG_action', "Action");
-define('OGP_LANG_steam_auto_update', "Steam Auto Update");
-define('OGP_LANG_stop', "Stop");
-define('OGP_LANG_start', "Start");
-define('OGP_LANG_restart', "Restart");
-define('OGP_LANG_command', "Command");
-define('OGP_LANG_add', "Add");
-define('OGP_LANG_remove', "Remove");
-define('OGP_LANG_now', "Now");
-define('OGP_LANG_schedule_new_job', "Schedule a new job");
-define('OGP_LANG_scheduled_jobs', "Scheduled jobs");
-define('OGP_LANG_there_are_no_scheduled_jobs', "There are no scheduled jobs");
-define('OGP_LANG_cron_events', "Cron Events");
-define('OGP_LANG_refresh_interval', "Refresh Interval");
-define('OGP_LANG_cron_no_servers_tied_to_account', "There are no servers directly assigned to your account.");
-define('OGP_LANG_cron_admin_link_display_text', "Cron Administration (All Servers)");
-define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "There aren't any servers currently configured in OGP.");
-define('OGP_LANG_bad_inputs', "Scheduled time contains invalid characters.");
+define('OGP_LANG_minute', "Λεπτό");
+define('OGP_LANG_hour', "Ώρα");
+define('OGP_LANG_day', "Ημέρα");
+define('OGP_LANG_month', "Μήνας");
+define('OGP_LANG_day_of_the_week', "Η / Εβδομάδα");
+define('OGP_LANG_action', "Ενέργεια");
+define('OGP_LANG_steam_auto_update', "Αυτόματη Ενημέρωση Steam");
+define('OGP_LANG_stop', "Σταμάτησε");
+define('OGP_LANG_start', "Ξεκίνησε");
+define('OGP_LANG_restart', "Επανεκίνησε");
+define('OGP_LANG_command', "Εντολή");
+define('OGP_LANG_add', "Πρόσθεστε");
+define('OGP_LANG_remove', "Αφαιρέστε");
+define('OGP_LANG_now', "Τώρα");
+define('OGP_LANG_schedule_new_job', "Προγραμματίστε μια νέα δουλειά.");
+define('OGP_LANG_scheduled_jobs', "Προγραμματισμένες δουλειές.");
+define('OGP_LANG_there_are_no_scheduled_jobs', "Δεν υπάρχουν προγραμματισμένες δουλειές.");
+define('OGP_LANG_cron_events', "Γεγονότα Cron");
+define('OGP_LANG_refresh_interval', "Διάστημα Ανανέωσης");
+define('OGP_LANG_cron_no_servers_tied_to_account', "Δεν υπάρχουν διακομιστές απευθείας αντιστοιχισμένοι στο λογαριασμό σας.");
+define('OGP_LANG_cron_admin_link_display_text', "Διαχείριση Cron (Όλοι Οι Διακομιστές)");
+define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "Δεν υπάρχουν διαμορφωμένοι διακομιστές στο OGP αυτή τη στιγμή.");
+define('OGP_LANG_bad_inputs', "Ο προγραμματισμένος χρόνος περιέχει μη έγκυρους χαρακτήρες.");
 ?>
