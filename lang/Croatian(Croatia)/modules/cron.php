a07aed25116b35890e084027e1cdc1758a12be77
own3mall <own3mall@gmail.com>
2018-11-27 03:18:51 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Croatian(Croatia)/modules/cron.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_minute', "Minute");
+define('OGP_LANG_hour', "Sati");
+define('OGP_LANG_day', "Dan");
+define('OGP_LANG_month', "Mjesec");
+define('OGP_LANG_day_of_the_week', "Dan/Tjedan");
+define('OGP_LANG_action', "Radnja");
+define('OGP_LANG_steam_auto_update', "Steam Automatsko Ažuriranje");
+define('OGP_LANG_stop', "Zaustaviti");
+define('OGP_LANG_start', "Pokrenuti");
+define('OGP_LANG_restart', "Osvježiti");
+define('OGP_LANG_command', "Naredba");
+define('OGP_LANG_add', "Dodati");
+define('OGP_LANG_remove', "Ukloniti");
+define('OGP_LANG_now', "Sada");
+define('OGP_LANG_schedule_new_job', "Zakazati novi Cron");
+define('OGP_LANG_scheduled_jobs', "Raspored Cron-ova");
+define('OGP_LANG_there_are_no_scheduled_jobs', "Nema zakazanih Cron-ova");
+define('OGP_LANG_cron_events', "Cron događaji");
+define('OGP_LANG_refresh_interval', "Osvježi interval");
+define('OGP_LANG_cron_no_servers_tied_to_account', "Nema izravno dodijeljenih servera na vašem računu.");
+define('OGP_LANG_cron_admin_link_display_text', "Cron Administracija (Svi Serveri)");
+define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "Nijedan server trenutno nije konfiguriran u OGP-u.");
+define('OGP_LANG_bad_inputs', "Zakazano Cron vrijeme sadrži nevažeće znakove.");
+?>
