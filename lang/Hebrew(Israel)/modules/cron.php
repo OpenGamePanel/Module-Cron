33ba23ff40b5b3f276e66362a40176afa4735270
own3mall <own3mall@gmail.com>
2020-04-26 14:34:57 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Hebrew(Israel)/modules/cron.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_minute', "Minute");
+define('OGP_LANG_hour', "Hour");
+define('OGP_LANG_day', "Day");
+define('OGP_LANG_month', "Month");
+define('OGP_LANG_day_of_the_week', "D/Week");
+define('OGP_LANG_action', "Action");
+define('OGP_LANG_steam_auto_update', "Steam Auto Update");
+define('OGP_LANG_stop', "Stop");
+define('OGP_LANG_start', "Start");
+define('OGP_LANG_restart', "Restart");
+define('OGP_LANG_command', "Command");
+define('OGP_LANG_add', "Add");
+define('OGP_LANG_remove', "Remove");
+define('OGP_LANG_now', "Now");
+define('OGP_LANG_schedule_new_job', "Schedule a new job");
+define('OGP_LANG_scheduled_jobs', "Scheduled jobs");
+define('OGP_LANG_there_are_no_scheduled_jobs', "There are no scheduled jobs");
+define('OGP_LANG_cron_events', "Cron Events");
+define('OGP_LANG_refresh_interval', "Refresh Interval");
+define('OGP_LANG_cron_no_servers_tied_to_account', "There are no servers directly assigned to your account.");
+define('OGP_LANG_cron_admin_link_display_text', "Cron Administration (All Servers)");
+define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "There aren't any servers currently configured in OGP.");
+define('OGP_LANG_bad_inputs', "Scheduled time contains invalid characters.");
+?>
