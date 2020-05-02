5331df85a623fbe75185ef5eb30f622da02dcb22
own3mall <own3mall@gmail.com>
2020-05-02 03:58:39 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Hebrew(Israel)/modules/cron.php
+++ lang/Hebrew(Israel)/modules/cron.php
@@ -22,27 +22,27 @@
  *
  */
 
-define('OGP_LANG_minute', "Minute");
-define('OGP_LANG_hour', "Hour");
-define('OGP_LANG_day', "Day");
-define('OGP_LANG_month', "Month");
-define('OGP_LANG_day_of_the_week', "D/Week");
-define('OGP_LANG_action', "Action");
-define('OGP_LANG_steam_auto_update', "Steam Auto Update");
-define('OGP_LANG_stop', "Stop");
-define('OGP_LANG_start', "Start");
-define('OGP_LANG_restart', "Restart");
-define('OGP_LANG_command', "Command");
-define('OGP_LANG_add', "Add");
-define('OGP_LANG_remove', "Remove");
-define('OGP_LANG_now', "Now");
-define('OGP_LANG_schedule_new_job', "Schedule a new job");
-define('OGP_LANG_scheduled_jobs', "Scheduled jobs");
-define('OGP_LANG_there_are_no_scheduled_jobs', "There are no scheduled jobs");
-define('OGP_LANG_cron_events', "Cron Events");
-define('OGP_LANG_refresh_interval', "Refresh Interval");
-define('OGP_LANG_cron_no_servers_tied_to_account', "There are no servers directly assigned to your account.");
-define('OGP_LANG_cron_admin_link_display_text', "Cron Administration (All Servers)");
-define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "There aren't any servers currently configured in OGP.");
-define('OGP_LANG_bad_inputs', "Scheduled time contains invalid characters.");
+define('OGP_LANG_minute', "דקה");
+define('OGP_LANG_hour', "שעה");
+define('OGP_LANG_day', "יום");
+define('OGP_LANG_month', "חודש");
+define('OGP_LANG_day_of_the_week', "יום\שבוע");
+define('OGP_LANG_action', "פעולה");
+define('OGP_LANG_steam_auto_update', "סטים - עדכון אוטומטי");
+define('OGP_LANG_stop', "עצור");
+define('OGP_LANG_start', "התחל");
+define('OGP_LANG_restart', "הפעלה מחדש");
+define('OGP_LANG_command', "פקודה");
+define('OGP_LANG_add', "הוסף");
+define('OGP_LANG_remove', "הסר");
+define('OGP_LANG_now', "עכשיו");
+define('OGP_LANG_schedule_new_job', "קבע עבודה חדשה");
+define('OGP_LANG_scheduled_jobs', "עבודה מתוזמנת");
+define('OGP_LANG_there_are_no_scheduled_jobs', "אין עבודות מתוזמנות");
+define('OGP_LANG_cron_events', "קרון אירועים");
+define('OGP_LANG_refresh_interval', "מרווח רענון");
+define('OGP_LANG_cron_no_servers_tied_to_account', "אין שרתים שהוקצו ישירות לחשבונך.");
+define('OGP_LANG_cron_admin_link_display_text', "ניהול קרון (כל השרתים)");
+define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "כרגע לא מוגדרים שרתים ב- OGP.");
+define('OGP_LANG_bad_inputs', "זמן מתוזמן מכיל תווים לא חוקיים.");
 ?>
