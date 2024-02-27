135f9b5229af7887a171e9aa4615713f34381b42
own3mall <own3mall@gmail.com>
2024-02-26 21:22:34 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Chinese(China)/modules/cron.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_minute', "分钟");
+define('OGP_LANG_hour', "小时");
+define('OGP_LANG_day', "天");
+define('OGP_LANG_month', "月");
+define('OGP_LANG_day_of_the_week', "星期几");
+define('OGP_LANG_action', "操作");
+define('OGP_LANG_steam_auto_update', "Steam自动更新");
+define('OGP_LANG_stop', "停止");
+define('OGP_LANG_start', "开始");
+define('OGP_LANG_restart', "重启");
+define('OGP_LANG_command', "命令");
+define('OGP_LANG_add', "添加");
+define('OGP_LANG_remove', "移除");
+define('OGP_LANG_now', "现在");
+define('OGP_LANG_schedule_new_job', "计划新任务");
+define('OGP_LANG_scheduled_jobs', "已计划的任务");
+define('OGP_LANG_there_are_no_scheduled_jobs', "没有计划的任务");
+define('OGP_LANG_cron_events', "Cron事件");
+define('OGP_LANG_refresh_interval', "刷新间隔");
+define('OGP_LANG_cron_no_servers_tied_to_account', "没有与账户关联的服务器");
+define('OGP_LANG_cron_admin_link_display_text', "Cron管理（所有服务哦）");
+define('OGP_LANG_cron_admin_no_ogp_servers_to_display', "OGP中当前没有配置任何服务器。");
+define('OGP_LANG_bad_inputs', "计划时间包含无效字符。");
+?>
